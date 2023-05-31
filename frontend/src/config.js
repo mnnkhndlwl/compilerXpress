f2555ebb983713a2f6de9fcd0fb70692e4d50502
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-06-01 00:48:49 +0530

    commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://compilerx.onrender.com";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
